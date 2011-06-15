ac17a5aa2e2e02e69cbea8387b3e060c384216c0
Namolovan Nicolae <adrenalinup@gmail.com>
2011-06-15 12:28:59 +0200

    PGCD is working

--- src/PGCD.vhd
+++ src/PGCD.vhd
@@ -31,17 +31,19 @@ END PGCD;
 
 
 ARCHITECTURE Montage OF PGCD IS
-    TYPE T_CMD_R IS (NOOP, LOAD, LOAD_B, COMPUTE);
+    TYPE T_CMD_R IS (NOOP, LOAD, LOAD_B, SWAP, SUB, COMPUTE);
 
 
     -- les operandes A et B
     -- on les etend sur 24 bits
-    SIGNAL CMD_AB :  T_CMD_R ; 
+    SIGNAL CMD_A,CMD_B :  T_CMD_R ; 
     SIGNAL R_INPUT,R_A,R_B:  STD_LOGIC_VECTOR (23 DOWNTO 0);
 
     -- l'adresse et le status
     SIGNAL CMD_Addr   :  T_CMD_R ; 
     SIGNAL CMD_Status :  T_CMD_R ; 
+	 SIGNAL CMD_Input :  T_CMD_R ; 
+	 
     SIGNAL R_Addr     :  STD_LOGIC_VECTOR ( 4 DOWNTO 0);
     SIGNAL R_Status   :  STD_LOGIC_VECTOR ( 2 DOWNTO 0);
 
@@ -49,17 +51,13 @@ ARCHITECTURE Montage OF PGCD IS
     SIGNAL CMD_Res :  T_CMD_R ; 
     SIGNAL R_Res   :  STD_LOGIC_VECTOR (23 DOWNTO 0);
 
-	 -- calcul pgcd
-    TYPE T_CMD_COMPUTE IS (NOOP, INIT, SWAP_AB, DECR_B_FROM_A, INIT_COMPUTE);
-    SIGNAL CMD_COMPUTE: T_CMD_COMPUTE;
-	 
     -- signaux intermediaires
-    SIGNAL a_tmp   : UNSIGNED (23 DOWNTO 0);
-    SIGNAL b_tmp   : UNSIGNED (23 DOWNTO 0);
-    SIGNAL c_tmp   : UNSIGNED (23 DOWNTO 0);
+    SIGNAL a_tmp   : SIGNED (23 DOWNTO 0);
+    SIGNAL b_tmp   : SIGNED (23 DOWNTO 0);
+    SIGNAL c_tmp   : SIGNED (23 DOWNTO 0);
 
     SIGNAL z_tmp   : STD_LOGIC;
-	 SIGNAL b_tmp_is_zero:  STD_LOGIC;
+	 SIGNAL fin_boucle:  STD_LOGIC;
 	 SIGNAL b_tmp_bigger_than_a:  STD_LOGIC;
     -- les bus in & out
     SIGNAL busin_addr   : STD_LOGIC_VECTOR( 4 DOWNTO 0);
@@ -70,7 +68,7 @@ ARCHITECTURE Montage OF PGCD IS
     SIGNAL busout_data  : STD_LOGIC_VECTOR(23 DOWNTO 0);
 
     --Description des états
-    TYPE STATE_TYPE IS (ST_READ_A,ST_READ_A_DO,
+    TYPE STATE_TYPE IS (ST_READ_A,ST_READ_A_DO,ST_READ_B_DO,
                         ST_READ_B, 
 		ST_COMPUTE,  -- the loop for compute
 		ST_SWAP_AB, -- swap AB
@@ -92,52 +90,55 @@ BEGIN
     busout(23 DOWNTO  0) <= busout_data  ;
 
 	 -- if (b==0) goto ST_WRITE;
-	 b_tmp_is_zero <= '1' WHEN b_tmp <= 0 ELSE '0';
+	 -- fin_boucle <= '1' WHEN R_A = R_B ELSE '0';
+	 fin_boucle <= '1' WHEN b_tmp = 0 ELSE '0';
 	 -- if (b>a) swap(a,b)
 	 b_tmp_bigger_than_a <= '1' WHEN b_tmp > a_tmp ELSE '0';
 	 
-    PROCESS (clk)
+    a_tmp <= SIGNED(R_A);
+	 b_tmp <= SIGNED(R_B);
+    
+	 PROCESS (clk)
     BEGIN IF clk'EVENT AND clk = '1' THEN
         -- R_Aaddr
         if    ( CMD_Addr = LOAD ) then
             R_Addr <= busin_addr;
         end if;
-        -- LOAD A 
-        if    ( CMD_AB = LOAD ) then
-				R_INPUT(23 DOWNTO 0) <= busin_data(23 DOWNTO 0);
-				R_A(23 DOWNTO 0) <= busin_data(23 DOWNTO 0);
-        end if;
-		  -- LOAD B
-        if    ( CMD_AB = LOAD_B ) then
-				R_INPUT(23 DOWNTO 0) <= busin_data(23 DOWNTO 0);
-				R_B(23 DOWNTO 0) <= busin_data(23 DOWNTO 0);
-        end if;
-		  -- INIT Compute
-		  if (CMD_COMPUTE = INIT_COMPUTE) then
-				a_tmp <= UNSIGNED(R_A);
-				b_tmp <= UNSIGNED(R_B);
-		  end if;
-		  -- SWAP A et B
-		  if ( CMD_COMPUTE = SWAP_AB ) then
-			   c_tmp <= a_tmp;
-				a_tmp <= b_tmp;
-				b_tmp <= c_tmp;
-		  end if;
-		  -- Decrement A by B
-		  if ( CMD_COMPUTE = DECR_B_FROM_A ) then
-				a_tmp <= UNSIGNED(a_tmp) - UNSIGNED(b_tmp);
-		  end if;
-        -- R_Res
-        --if    ( CMD_Res = LOAD ) then
-        R_Res <= std_logic_vector(a_tmp);
-        --end if;
-        -- R_Status
+		  -- R_Status
         if    ( CMD_Status = LOAD ) then
             R_Status <= busin_status;
         end if;
-		  -- 
+        if    ( CMD_Input = LOAD ) then
+            R_input <= busin_data;
+        end if;
+
+        -- LOAD A 
+        if    ( CMD_A= LOAD ) then
+				R_A <= R_input;
+			elsif ( CMD_A = SWAP ) then
+			   R_A <= R_B;
+         elsif ( CMD_A = SUB ) then
+	         R_A <= std_logic_vector(a_tmp - b_tmp);
+		   else
+				R_A <= R_A;
+        end if;
+		  
+        -- LOAD B
+        if    ( CMD_B= LOAD ) then
+				R_B <= R_input;
+			elsif ( CMD_B = SWAP ) then
+			   R_B <= R_A;
+		   else
+				R_B <= R_B;
+        end if;
+		  
+		  if (CMD_Res = LOAD) then
+				R_Res <= R_A;
+		  end if;
+			
+	
     END IF; END PROCESS;
-    
+    	 
     busout_addr      <= R_Addr;
 
 	 busout_status(2) <= '1' when state=ST_WRITE_COPY else '0';
@@ -164,23 +165,26 @@ BEGIN
                   ELSIF busin_valid  = '1' and busin_addr /= "00011" THEN
                       state <= ST_WRITE_COPY;
                   END IF; 
-
+						
               WHEN ST_READ_A_DO =>
                   state <= ST_READ_B;
 						
               WHEN ST_READ_B =>
                   IF busin_valid  = '1' and busin_addr = "00011" THEN
-                      state <= ST_INIT_COMPUTE;
+                      state <= ST_READ_B_DO;
                   ELSIF busin_valid  = '1' and busin_addr /= "00011" THEN
                       state <= ST_WRITE_COPY;
                   END IF; 
+						
+              WHEN ST_READ_B_DO =>
+                  state <= ST_INIT_COMPUTE;
 
 				  WHEN ST_INIT_COMPUTE =>
 						state <= ST_COMPUTE;		
 						
 				  WHEN ST_COMPUTE =>
-						IF b_tmp_is_zero ='1' THEN
-							 state <= ST_WRITE_PGCD;
+						IF fin_boucle = '1' THEN
+							 state <= ST_LOAD_PGCD;
 						ELSIF b_tmp_bigger_than_a='1' THEN
 							state <= ST_SWAP_AB;
 						ELSIF b_tmp_bigger_than_a='0' THEN
@@ -228,22 +232,26 @@ BEGIN
     WITH state  SELECT CMD_Status <=
          LOAD   WHEN   ST_READ_A,
 			LOAD   WHEN   ST_READ_B,
+         NOOP   WHEN   OTHERS;
+
+		WITH state  SELECT CMD_input <=
+         LOAD   WHEN   ST_READ_A,
+			LOAD   WHEN   ST_READ_B,
          NOOP   WHEN   OTHERS; 
 
-    WITH state  SELECT CMD_AB <=
-         LOAD   WHEN   ST_READ_A,
-			LOAD_B   WHEN   ST_READ_B,
+    WITH state  SELECT CMD_A <=
+         LOAD   WHEN   ST_READ_A_DO,
+			SWAP   WHEN   ST_SWAP_AB,
+			SUB    WHEN   ST_DECR_B_FROM_A,
+         NOOP   WHEN   OTHERS; 
+	
+	 WITH state  SELECT CMD_B <=
+         LOAD   WHEN   ST_READ_B_DO,
+			SWAP   WHEN   ST_SWAP_AB,
          NOOP   WHEN   OTHERS; 
 
     WITH state  SELECT CMD_Res <=
-         COMPUTE   WHEN   ST_Compute,
 			LOAD WHEN ST_LOAD_PGCD,
-         NOOP   WHEN   OTHERS; 
-	
-    WITH state  SELECT CMD_COMPUTE <=
-			INIT_COMPUTE WHEN	ST_INIT_COMPUTE,
-         SWAP_AB   WHEN   ST_SWAP_AB,
-			DECR_B_FROM_A WHEN ST_DECR_B_FROM_A,
          NOOP   WHEN   OTHERS; 
 	
 
